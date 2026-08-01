759cdd8e9446f6db77de0923c75d885fc54e8b7d
Mafaka77 <zuala4@gmail.com>
2026-08-02 01:37:36 +0530

    asdasd

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -143,7 +143,7 @@ router.beforeEach((to, from, next) => {
   const token = localStorage.getItem('token')
   if (to.meta.requiresAuth && !token) {
     next('/login')
-  } else if (to.path === '/login' && token) {
+  } else if ((to.path === '/login' || to.path === '/') && token) {
     next('/dashboard')
   } else {
     next()
