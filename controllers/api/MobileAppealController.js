35cac08eb886528cd8e74fdd5b7df8cffdaee6d0
Mafaka77 <zuala4@gmail.com>
2026-08-11 11:30:44 +0530

    appeal user id

--- controllers/api/MobileAppealController.js
+++ controllers/api/MobileAppealController.js
@@ -147,6 +147,7 @@ module.exports = {
       const body  = request.body || {};
       const query = request.query || {};
       const reason = body.reason || query.reason;
+      const userId = request.user.id;
 
       if (!reason) {
         return reply.send({
@@ -176,7 +177,7 @@ module.exports = {
 
       const appeal = await AppealAttendance.create({
         attendance_id: attendance.id,
-        user_id:       attendance.user_id,
+        user_id:       userId,
         office_id:     attendance.office_id,
         start_date:    attendance.signin_at,
         type:          'late_reason',
